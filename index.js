b5ff1d372238511198a9c5e372d6d374730e607c
EmmaBlueJeans <119965766+EmmaBlueJeans@users.noreply.github.com>
2023-03-31 10:52:34 -0700

    html and js updates
    made adjustments to html for js functions to display the weekly forecast with dummy data.  few css changes for formatting

--- index.js
+++ index.js
@@ -1,38 +1,97 @@
-let now = new Date();
-let time = document.querySelector("ul li:first-child");
-let hours = now.getHours();
-let minutes = now.getMinutes();
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+  //let time = document.querySelector("ul li:first-child");
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
+  // time.innerHTML = `${day},  ${hours}:${minutes}`;
+}
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
 
-time.innerHTML = `${day},  ${hours}:${minutes}`;
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `              <div class="col-2">
+                <div class="weather-forecast-date">${day}</div>
+                <img
+                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+                  alt="broken-clouds-day"
+                  width="42"
+                />
+                <div class="weather-forecast-temperature">
+                  <span class="weather-forecast-temperature-max"> 18° </span>
+                  <span class="weather-forecast-temperature-min"> 12° </span>
+                </div>
+              </div>
+              `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "da23aofdf95d0c460f65b6cf813aa6tf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=imperial`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayWeatherCondition(response) {
-  document.querySelector("h1").innerHTML = response.data.name;
-  document.querySelector("#temp").innerHTML = Math.round(
-    response.data.main.temp
-  );
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  //let dateElement = document.querySelector ("#date");
+  let iconElement = document.querySelector("#icon");
+  console.log(response.data);
 
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(
-    response.data.wind.speed
-  );
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+  fahrenheitTemperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.spped * 3.6);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  //dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  //iconElement.setAttribute(
+  //  "src",
+  //  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  //);
+  //iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.icon);
+
+  getForecast(response.data.coordinates);
 }
 
 function searchCity(city) {
-  let apiKey = "311f1f45fee82242ab4086372ab360f5";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "da23aofdf95d0c460f65b6cf813aa6tf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
@@ -56,15 +115,22 @@ function getCurrentLocation(event) {
 
 function convertToCelsius(event) {
   event.preventDefault();
-  var temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML = 66;
+  //var temperatureElement = document.querySelector("#temperature");
+  //temperatureElement.innerHTML = 66;
+  let temperatureElement = document.querySelector("#temperature");
+  let celsiusTemp = (fahrenheitTemp - 32) / 1.8;
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
 }
 
 function convertToFahrenheit(event) {
   event.preventDefault();
-  var temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML = 19;
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+  //var temperatureElement = document.querySelector("#temperature");
+  // temperatureElement.innerHTML = 19;
 }
+
+let fahrenheitTemp = null;
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
@@ -77,3 +143,5 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+searchCity("Seattle");
